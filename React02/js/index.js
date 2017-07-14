e5cd45fb725b5e89b4fc339b98f9af4f0300b40c
hwr <1092021277@qq.com>
2017-07-14 14:05:53 +0800

    add use of ref

--- React02/js/index.js
+++ React02/js/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../css/common.css';
+import CustomTextInput from  './refTest'
 
 import Heard from '../component/Heard/index'    //如果配置文件没有配置resolve，则此处的引入需要些后缀
 
@@ -9,6 +10,8 @@ ReactDOM.render(
     <div>
         <h1 className="c-red">Titile</h1>
         <Heard></Heard>
+
+        <CustomTextInput/>
     </div>,
     document.getElementById('root')
 );
